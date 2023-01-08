474c852f3911215bf32634eaf1f3e6fe2e0cb5a1
fahaad-abbadi <al.fahaadabbadi@gmail.com>
2023-01-08 21:49:56 +0530

    some commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
+//Header Component
+const Header = (props) => {
+  console.log(props); //empty object, {}
+
   return (
-    <div className="App">
-    </div>
-  );
+    <header>
+      <div className="header-wrapper">
+        <h1>{props.welcome}</h1>
+        <h2>{props.welcome}</h2>
+        <h3>{props.welcome}</h3>
+        <h4>{props.welcome}</h4>
+        <h5>{props.welcome}</h5>
+        <h6>{props.welcome}</h6>
+      </div>
+    </header>
+  )
 }
+
+//The app, or the parent or the container component
+//Functional component
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header welcome='This is some ReactJS thing' />
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
 
 export default App;
